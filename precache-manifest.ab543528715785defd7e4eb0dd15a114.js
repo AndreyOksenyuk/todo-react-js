f848d152cd10466439fbdba479f99d87a8cee1eb
AndreyOksenyuk <oksenyuk.andrey@gmail.com>
2020-08-02 16:29:48 +0300

    Updates

---
+++ precache-manifest.ab543528715785defd7e4eb0dd15a114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace485f985f5f3cba46b637f30625404",
+    "url": "/todo-react-js/index.html"
+  },
+  {
+    "revision": "b6b65214e413e52c778f",
+    "url": "/todo-react-js/static/css/main.410a34d1.chunk.css"
+  },
+  {
+    "revision": "70be8200b4f8a553b133",
+    "url": "/todo-react-js/static/js/2.74154f7a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-react-js/static/js/2.74154f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b65214e413e52c778f",
+    "url": "/todo-react-js/static/js/main.effa4bab.chunk.js"
+  },
+  {
+    "revision": "a22453abfb4dcc640988",
+    "url": "/todo-react-js/static/js/runtime-main.c17419a2.js"
+  }
+]);
